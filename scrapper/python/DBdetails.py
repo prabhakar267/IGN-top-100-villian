236b96358b5704857da1b8f28e5cc9163db0ffda
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-04-16 00:00:00 +0530

    database details removed

--- scrapper/python/DBdetails.py
+++ scrapper/python/DBdetails.py
@@ -2,9 +2,9 @@
 # @Author: prabhakar
 # @Date:   2016-04-18 01:52:57
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-04-18 02:04:07
+# @Last Modified time: 2016-04-18 02:04:53
 
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASS = "696163"
-DB_NAME = "top_villians_comics"
+DB_HOST = "XXXX"
+DB_USER = "XXXX"
+DB_PASS = "XXXX"
+DB_NAME = "XXXX"
